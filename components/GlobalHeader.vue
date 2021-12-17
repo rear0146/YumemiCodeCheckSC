44ae1cfce47028f9b670fb396f19d83bc3d36255
rear0146 <shiraishi.chihiro@uec.ac.jp>
2021-12-17 20:28:28 +0900

    Add components

---
+++ components/GlobalHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        <h1>
+            都道府県別人口推移
+        </h1>
+    </header>
+</template>
---
+++ components/Population.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <h2>都道府県を選択</h2>
+
+    </div>
+    <div>
+      <h2>グラフ</h2>
+
+    </div>
+  </div>
+</template>
