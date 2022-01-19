33aa1d207a128797d3a8fd90294db5000de2b132
rear0146 <shiraishi.chihiro@uec.ac.jp>
2022-01-19 17:45:26 +0900

    add default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <GlobalHeader/>
+    <div class="MainContents">
+      <Nuxt />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <GlobalHeader />
     <DisplayPopulation />
   </div>
 </template>
