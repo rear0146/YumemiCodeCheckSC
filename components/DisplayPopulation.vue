a9cd18fe574490015c3c53eee73c1a69493b3849
rear0146 <shiraishi.chihiro@uec.ac.jp>
2022-01-19 17:28:10 +0900

    add PrefList component

--- components/DisplayPopulation.vue
+++ components/DisplayPopulation.vue
@@ -2,29 +2,13 @@
   <div class="container">
     <div class="section">
       <h2>都道府県を選択</h2>
-      <ul class="prefList">
-        <!-- 都道府県の一覧 -->
-        <li v-for="pref in prefs" :key="pref.prefCode">
-          <label>
-            <input
-              :id="pref"
-              v-model="selectedPrefs"
-              type="checkbox"
-              :value="pref"
-              @change="togglePref(pref)"
-            />
-            <div :style="prefNameStyle(pref.prefCode)" class="prefName">
-              {{ pref.prefName }}
-            </div>
-          </label>
-        </li>
-      </ul>
+      <PrefList class="prefList" />
     </div>
     <div class="section">
       <h2>グラフ</h2>
       <!-- 選択された都道府県の人口をチャートで表示 -->
       <LineChart
-        v-if="selectedPrefs.length"
+        v-if="$store.state.population.length"
         :chart-data="chartData"
         :options="chartOptions"
         class="chart"
@@ -34,11 +18,14 @@
 </template>
 
 <script>
+import PrefList from './PrefList.vue';
 export default {
   name: 'DisplayPopulation',
+  components: {
+    PrefList
+  },
   data() {
     return {
-      selectedPrefs: [],
       chartOptions: {
         maintainAspectRatio: false,
         scales: {
@@ -61,37 +48,10 @@ export default {
     }
   },
   computed: {
-    prefs() {
-      return this.$store.getters.getPref
-    },
     chartData() {
       return this.$store.getters.getChartData
     },
-  },
-  mounted() {
-    this.fetchPref()
-  },
-  methods: {
-    fetchPref() {
-      // 都道府県の読み込み
-      this.$store.dispatch('fetchPref')
-    },
-    prefNameStyle(prefCode) {
-      // 各都道府県固有の色
-      const RGB = this.$prefCodeToColor(prefCode)
-      return {
-        '--color': `rgba(${RGB[0]},${RGB[1]},${RGB[2]},1)`,
-      }
-    },
-    togglePref(pref) {
-      // 各都道府県が選択/選択解除されたときの処理
-      if (this.selectedPrefs.includes(pref)) {
-        this.$store.dispatch('fetchPopulation', pref)
-      } else {
-        this.$store.commit('removePref', pref)
-      }
-    },
-  },
+  }
 }
 </script>
 
@@ -102,52 +62,6 @@ export default {
   list-style-type: none;
   margin: 0 auto;
   padding: 0;
-}
-
-.prefList li {
-  display: inline-block;
-  margin: 5px 3px;
-}
-
-
-.prefList li label {
-  cursor: pointer;
-}
-
-.prefList li input[type="checkbox"] {
-  filter: alpha(opacity=0);
-  -moz-opacity:0;
-  opacity:0;
-  -webkit-appearance: none;
-  appearance: none;
-  position: absolute;
-}
-
-/* ----- 一覧の各都道府県 ----- */
-.prefName {
-  padding: 2px 10px;
-  border: 1px solid var(--color);
-  color: var(--color);
-  border-radius: 20px;
-  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.4);
-}
-
-.prefName:hover {
-  border-radius: 20px;
-  transition-duration: 0.2s;
-  box-shadow: 5px 3px 5px rgba(0, 0, 0, 0.4);
-}
-
-.prefList li input:focus ~ .prefName {
-  border-radius: 20px;
-  transition-duration: 0.2s;
-  box-shadow: 5px 3px 5px rgba(0, 0, 0, 0.4);
-}
-
-.prefList li input:checked ~ .prefName {
-  color: #ffffff;
-  background-color: var(--color);
-  box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.3) inset;
 }
 
 /* ----- チャート ----- */
---
+++ components/PrefList.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+        <ul class="prefList">
+        <!-- 都道府県の一覧 -->
+        <li v-for="pref in prefs" :key="pref.prefCode">
+          <label>
+            <input
+              :id="pref"
+              v-model="selectedPrefs"
+              type="checkbox"
+              :value="pref"
+              @change="togglePref(pref)"
+            />
+            <div :style="prefNameStyle(pref.prefCode)" class="prefName">
+              {{ pref.prefName }}
+            </div>
+          </label>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PrefList',
+    data() {
+        return {
+            selectedPrefs: [],
+        }
+    },
+    computed: {
+    prefs() {
+      return this.$store.getters.getPref
+    }
+  },
+  mounted() {
+    this.fetchPref()
+  },
+  methods: {
+    fetchPref() {
+      // 都道府県の読み込み
+      this.$store.dispatch('fetchPref')
+    },
+    prefNameStyle(prefCode) {
+      // 各都道府県固有の色
+      const RGB = this.$prefCodeToColor(prefCode)
+      return {
+        '--color': `rgba(${RGB[0]},${RGB[1]},${RGB[2]},1)`,
+      }
+    },
+    togglePref(pref) {
+      // 各都道府県が選択/選択解除されたときの処理
+      if (this.selectedPrefs.includes(pref)) {
+        this.$store.dispatch('fetchPopulation', pref)
+      } else {
+        this.$store.commit('removePref', pref)
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.prefList {
+  list-style-type: none;
+}
+
+.prefList li {
+  display: inline-block;
+  margin: 5px 3px;
+}
+
+
+.prefList li label {
+  cursor: pointer;
+}
+
+.prefList li input[type="checkbox"] {
+  filter: alpha(opacity=0);
+  -moz-opacity:0;
+  opacity:0;
+  -webkit-appearance: none;
+  appearance: none;
+  position: absolute;
+}
+
+/* ----- 一覧の各都道府県 ----- */
+.prefName {
+  padding: 2px 10px;
+  border: 1px solid var(--color);
+  color: var(--color);
+  border-radius: 20px;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.4);
+}
+
+.prefName:hover {
+  border-radius: 20px;
+  transition-duration: 0.2s;
+  box-shadow: 5px 3px 5px rgba(0, 0, 0, 0.4);
+}
+
+.prefList li input:focus ~ .prefName {
+  border-radius: 20px;
+  transition-duration: 0.2s;
+  box-shadow: 5px 3px 5px rgba(0, 0, 0, 0.4);
+}
+
+.prefList li input:checked ~ .prefName {
+  color: #ffffff;
+  background-color: var(--color);
+  box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.3) inset;
+}
+
+</style>
