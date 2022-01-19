9a4c28502369658472960d713e533572d479b4ec
rear0146 <shiraishi.chihiro@uec.ac.jp>
2022-01-19 23:07:44 +0900

    エラーページ追加

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>{{ error.statusCode }}</h1>
+    <h1>{{ errorMessage }}</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  props: {
+    error: { type: Object, default () { return {} } }
+  },
+  computed: {
+    errorMessage () {
+      if (this.error.statusCode === 404) {
+        return 'ページが存在しません'
+      }
+      if (this.error.message) {
+        return this.error.message
+      }
+      if (this.error.statusCode === 429) {
+          return 'APIへのリクエスト回数が多く、 1秒あたりのリクエスト平均数・リクエスト数/日のうち、どちらかの制限を超えました。リトライしてください'
+      }
+      return 'エラーが発生しました'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+</style>
